9003ce65e095d4f4c21b447566a4deb5c5741fef
Anthony Holder <anthonyh202x@gmail.com>
2020-01-05 17:09:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,11 +2,27 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 require 'pry'
 
-def total_gross
+def gross_for_director(director_data)
 i = 0
 total = 0 
-  
-total   
+  while i < director_data[:movies].size 
+  total += director_data[:movies][i][:worldwide_gross] 
+  i += 1
+  end   
+  total 
+  #binding.pry
+end
+
+def directors_totals(nds)
+i = 0 
+totals = {}
+  while i < nds.size
+  director = nds[i]
+  totals[director[:name]] = gross_for_director(director)
+  #binding.pry
+  i += 1
+  end 
+totals
 end 
 
-
+binding.pry
